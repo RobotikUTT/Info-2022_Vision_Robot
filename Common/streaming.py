a97f665eae3e529c1d16c41b513de1fa5af39d69
Gregory <gregory.holder76@gmail.com>
2021-12-02 14:12:46 +0000

    Streaming with multiple clients

--- Common/streaming.py
+++ Common/streaming.py
@@ -3,39 +3,80 @@ import socket
 import pickle
 import struct
 import simplejpeg
+import calibration
 
-HOST = ''
-PORT = 8089
-
-cap = cv2.VideoCapture(0)
+from time import sleep
 
 payload_size = struct.calcsize("L") ### CHANGED
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print('Socket created')
+cap = cv2.VideoCapture(0)
 
-s.bind((HOST, PORT))
-print('Socket bind complete')
-s.listen(10)
-print('Socket now listening')
+class VideoStreamer():
+    def __init__(self, port):
+        self.port = port
 
-conn, addr = s.accept()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setblocking(0)
+        self.socket.bind(('', self.port))
+        self.socket.listen(10)
+        
+        self.connections = []
 
-data = b'' ### CHANGED
+    def checkConnections(self):
+        try:
+            conn, addr = self.socket.accept()
+            self.connections.append(conn)
+            print(f"new connection. now {len(self.connections)} connections")
+            print(conn)
 
+        except BlockingIOError:
+            pass
+
+    def sendFrame(self, frame=None):
+        frame = cv2.resize(frame, (720, 480))
+
+        # Serialize frame
+        data = simplejpeg.encode_jpeg(frame, 50)
+        data = pickle.dumps(data)
+
+        # Send message length first
+        message_size = struct.pack("L", len(data)) ### CHANGED
+
+        for conn in self.connections.copy():
+            try:
+                conn.sendall(message_size + data)
+            except (ConnectionResetError, BrokenPipeError):
+                conn.close()
+                self.connections.remove(conn)
+
+    def closeAll(self):
+        for c in self.connections:
+            c.shutdown(2)
+            c.close()
 
 while True:
-    ret,frame = cap.read()
-    frame = cv2.resize(frame, (720, 480))
+    try:
+        v = VideoStreamer(8089)
+        print("ready")
+        break
+    except OSError:
+        print(".", end="")
+        sleep(1)
 
-    # Serialize frame
-    data = simplejpeg.encode_jpeg(frame, 1)
-    data = pickle.dumps(data)
 
-    # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
+calib = calibration.CameraCalibration.load("config.json")
 
-    # Then data
-    conn.sendall(message_size + data)
 
-    
+
+try:
+    while True:
+        v.checkConnections()
+        ret, frame = cap.read() 
+        frame = cv2.resize(frame, calib.DIM)
+        frame = calibration.undistort(frame, calib)
+
+        v.sendFrame(frame)
+
+except KeyboardInterrupt:
+    print("quitting")
+    v.closeAll()

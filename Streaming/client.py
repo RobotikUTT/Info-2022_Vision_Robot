7c2f7ccebef73933fcdae355c38b55cdd7176d19
Gregory Holder <gregory.holder76@gmail.com>
2021-12-03 12:01:41 +0100

    Wait for server to become available

--- Streaming/client.py
+++ Streaming/client.py
@@ -1,42 +1,51 @@
+#!/bin/python3
 import cv2
 import socket
 import pickle
 import struct
+from time import sleep
 
 import simplejpeg
 
 
-
-# cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('192.168.43.184',8089))
-
-payload_size = struct.calcsize("=L") ### CHANGED
-
-data = b'' ### CHANGED
-
 while True:
-    # Retrieve message size
-    while len(data) < payload_size:
-        data += clientsocket.recv(4096)
+    clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("=L", packed_msg_size)[0] ### CHANGED
+    while True:
+        try:
+            clientsocket.connect(('192.168.43.184',1234))
+            break
+        except ConnectionRefusedError:
+            print("waiting for server")
+            sleep(1)
 
-    # Retrieve all data based on message size
-    while len(data) < msg_size:
-        data += clientsocket.recv(4096)
 
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
+    payload_size = struct.calcsize("=L") ### CHANGED
 
-    # Extract frame
-    frame = pickle.loads(frame_data)
-    frame = simplejpeg.decode_jpeg(frame)
+    data = b'' ### CHANGED
 
-    frame = cv2.resize(frame, (1080, 720))
+    while True:
+        # Retrieve message size
+        while len(data) < payload_size:
+            data += clientsocket.recv(4096)
 
-    # Display
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("=L", packed_msg_size)[0] ### CHANGED
+
+        # Retrieve all data based on message size
+        while len(data) < msg_size:
+            data += clientsocket.recv(4096)
+
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+
+        # Extract frame
+        frame = pickle.loads(frame_data)
+        frame = simplejpeg.decode_jpeg(frame)
+
+        frame = cv2.resize(frame, (1080, 720))
+
+        # Display
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
